a2e440d0c016f68229466ec861be194d1ba749ab
jason1466 <jason1466@users.noreply.github.com>
2019-06-18 17:35:50 -0400

    enable CORS

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const appOptions = {};
+  const app = await NestFactory.create(AppModule, appOptions);
+  // app.setGlobalPrefix('api');
+  app.enableCors({
+    // origin: ‘http://localhost:4200’
+    // origin?: boolean | string | RegExp | (string | RegExp)[] | CustomOrigin;
+    // methods?: string | string[];
+    // allowedHeaders?: string | string[];
+    // exposedHeaders?: string | string[];
+    // credentials?: boolean;
+    // maxAge?: number;
+    // preflightContinue?: boolean;
+    // optionsSuccessStatus?: number;
+  });
   const port = process.env.PORT || 3000;
   await app.listen(port, () => {
     console.log('API is listening on port ' + port);
